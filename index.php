5593c66a59a8671d25dd3de1b20b39946585253f
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-01-25 15:39:07 +0100

    Mis en place du HomeController

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <?php
 
-echo "hello world";
-
-$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$page = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $routes = [
     'database' => 'config/database.php',
-
+    'index' => 'app/controllers/homeController.php',
 ];
 
 /** @define "$routes" "/" */
 
-if (isset($action)) {
-    if (array_key_exists($action, $routes)) {
-        require $routes[$action];
+if (isset($page)) {
+    if (array_key_exists($page, $routes)) {
+        require $routes[$page];
     } else {
         header("HTTP/1.0 404 Not Found");
-        require $routes['error'];
+        require $page['error'];
         echo $buffer;
     }
 } else {
-    require $routes['index.php'];
+    require $routes['index'];
 }
 
 include $routes['database'];
