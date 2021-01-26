290174ccfdc86b2dd9fc6d58a26c860a39bfb67d
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-01-26 10:30:33 +0100

    function lastBlogPost with the SQL request

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -2,4 +2,5 @@
 
 require 'app/persistences/blogPostData.php';
 //echo "hello world";
-var_dump(lastBlogPosts($pdo));
+//var_dump(lastBlogPosts($pdo));
+var_dump($lastBlogPosts);
--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -1,8 +1,12 @@
 <?php
 function lastBlogPosts(PDO $pdo) : array
 {
-    return [];
+    return [$pdo ->query('select posts.title, posts.text, authors.name, authors.firstname from posts inner join authors ON posts.authors_id = authors.id order by posts.date_begin limit 10')];
 }
+
+$lastBlogPosts = lastBlogPosts($pdo);
+
+//select posts.title, posts.text, authors.name, authors.firstname from posts inner join authors ON posts.authors_id = authors.id order by posts.date_begin limit 10
 
 /* try {
     foreach ($pdo->query('select text, title, authors from posts inner join  authors ON `posts`.`authors_id` = `authors`.`id`') as $array) {
