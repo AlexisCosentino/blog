6c13f95532f35886513bad4c2b03adf8d4a4c8f4
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-01-29 09:52:42 +0100

    app/controllers/blogPostCreateController.php

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -19,27 +19,22 @@ function commentsByBlogPost(PDO $pdo, $posts_id) : array
     return $result -> fetchAll();
 }
 
-//function blogPostCreate(PDO $pdo, $createTitle, $createText, $createdateB, $createdateE, $createImportant, $authors_id) : array
-/*function blogPostCreate(PDO $pdo, $inputs) : array
-{
-    $result = $pdo->prepare("insert into posts (title, text, important, authors_id) value ($inputs)");
-    $result ->execute();
-    $resultfunction = $result-> fetch(PDO::FETCH_ASSOC);
-    return $resultfunction;
-    // $result = $pdo ->query("insert into posts (title, text, important, authors_id) value ($createTitle, $createText, $createdateB, $createdateE, $createImportant, $authors_id)");
-    //return $result -> fetch(PDO::FETCH_ASSOC);
-}
-*/
-//select posts.title, posts.text, authors.name, authors.firstname from posts inner join authors ON posts.authors_id = authors.id order by posts.date_begin limit 10
 
+
+
+//Creer un post
 function blogPostCreate(PDO $pdo, $title, $text, $datebegin, $dateend, $important, $author) : bool
 {
-    $result = $pdo->query("insert into posts (title, text, date_begin, date_end, important, authors_id) value ('$title', '$text', '$datebegin', '$dateend', '$important', '$author')");
-    return $result->fetch(PDO::FETCH_ASSOC);
+    $result = $pdo ->prepare('insert into posts (title, text, date_begin, date_end, important, authors_id) value (?, ?, ?, ?, ?, ?)');
+    $result->bindParam(1, $title, PDO::PARAM_STR);
+    $result->bindParam(2, $text, PDO::PARAM_STR);
+    $result->bindParam(3, $datebegin, PDO::PARAM_STR);
+    $result->bindParam(4, $dateend, PDO::PARAM_STR);
+    $result->bindParam(5, $important, PDO::PARAM_STR);
+    $result->bindParam(6, $author, PDO::PARAM_STR);
+    return $result ->execute();
+
 }
-
-
-
 
 
 
